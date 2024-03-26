11a2d55c136ea259fec180c92f90e058876133fb
Timiemmy <faith.bolanle@yahoo.com>
2024-03-26 10:51:07 +0100

    added account forms and modify admin

--- TodoPrj/settings.py
+++ TodoPrj/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-AUTH_USER_MODEL = "accounts.CustomUser"
+AUTH_USER_MODEL = "account.CustomUser"
 
 GRAPHENE = {
     "SCHEMA": "blog_api.schema.schema",
--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'name']
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ("name",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ("name",)}),)
